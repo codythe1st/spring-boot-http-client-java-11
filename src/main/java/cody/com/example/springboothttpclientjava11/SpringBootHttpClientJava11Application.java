bbdfd27c26cd0d87eb5bd10705c3d2ad1cc4fa54
cody benson <codythe1st@gmail.com>
2020-10-11 11:37:35 -0500

    initial commit for http project

---
+++ src/main/java/cody/com/example/springboothttpclientjava11/SpringBootHttpClientJava11Application.java
@@ -0,0 +1,13 @@
+package cody.com.example.springboothttpclientjava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHttpClientJava11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHttpClientJava11Application.class, args);
+	}
+
+}
---
+++ src/test/java/cody/com/example/springboothttpclientjava11/SpringBootHttpClientJava11ApplicationTests.java
@@ -0,0 +1,13 @@
+package cody.com.example.springboothttpclientjava11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHttpClientJava11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
